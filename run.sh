2f14e868bbc9a7585f104833de3d96cbd9a832dc
nctiggy <nctiggy@gmail.com>
2024-05-24 15:03:20 -0700

    updates

--- run.sh
+++ run.sh
@@ -12,6 +12,16 @@ source <(curl -fsSL https://raw.githubusercontent.com/mrbaseman/parse_yaml/maste
 curl -o /tmp/tools.yaml https://raw.githubusercontent.com/nctiggy/prereq_tool/main/tools.yaml > /dev/null 2>&1
 
 ########################################################
+# Set color vars
+########################################################
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+cyan=`tput setaf 6`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+
+########################################################
 # Parse out semver to int (eg. 18.24.123 -> 018024123
 ########################################################
 function version { echo "$@" | awk -F. '{ printf("%03d%03d%03d\n",
@@ -21,11 +31,11 @@ function version { echo "$@" | awk -F. '{ printf("%03d%03d%03d\n",
 # Check for existance of binary, that it is executable
 # and minimum version
 ########################################################
-binary_checks() {
+binary_good() {
     local __resultvar=$1
     local tool_binary=$2 version_command=$3
     printf "${cyan}Checking ${tool_binary} version.... "
-    command -v $tool_binary > /dev/null 2>&1 && [ -x $(command -v $tool_binary) ]
+    command -v $tool_binary > /dev/null 2>&1 && [ -x "$(command -v $tool_binary)" ]
     if [ $? -eq 0 ]
     then
       local result=$(eval "${version_command}")
@@ -35,22 +45,66 @@ binary_checks() {
     return 1
 }
 
-eval $(parse_yaml /tmp/tools.yaml)
+########################################################
+# This is the main function that loops through the tools
+# yaml file and checks for existance and version
+########################################################
 check_tools() {
   for t in $tools_
   do
+    current_version=0
     local tool=$(eval echo "\${t}_")
     local name="${tool}name"
     local command="${tool}version_command"
     local minimum_version="${tool}minimum_version"
-    binary_checks test "${!name}" "${!command}"
+    binary_good current_version "${!name}" "${!command}" && version_good ${!minimum_version} ${current_version}
     if [[ $? -eq 0 ]]
     then
-      echo "success ${test}"
+      printf "${green}${current_version}\n${reset}"
     else
-      echo "failure ${test}"
+      failed_software=( "${failed_software[@]}" "${!name}" )
+      if [ $current_version == "0" ]
+      then
+        printf "${red}Not Installed"
+      else
+        printf "${red}${current_version}"
+      fi
+      printf  " | minimum ver. ${!minimum_version}\n${reset}"
     fi
   done
 }
 
+########################################################
+# Function to validate the currently installed version
+# meets or exceeds the minimum value required
+########################################################
+version_good() {
+    local min_ver=$1 curr_ver=$2 re='^[0-9]+$'
+    [[ $(version $curr_ver) =~ $re ]] && [ $(version $curr_ver) -ge $(version $min_ver) ]
+    if [[ $? -eq 0 ]]
+    then
+      return 0
+    fi
+    return 1
+}
+
+failed_software=()
+tools_yaml="/tmp/tools.yaml"
+current_version=0
+
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+    "--tools"|"-t")
+      tools_yaml=$2
+      shift
+      shift
+      ;;
+    *)
+      ;;
+  esac
+done
+eval $(parse_yaml "${tools_yaml}")
 check_tools
+echo ${failed_software[*]}
