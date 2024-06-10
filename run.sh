d88c71a050f4981692904de19f05799e4bff37cf
nctiggy <nctiggy@gmail.com>
2024-06-10 12:35:00 -0700

    updated functionality

--- run.sh
+++ run.sh
@@ -24,6 +24,10 @@ reset=`tput sgr0`
 cyan=`tput setaf 6`
 blue=`tput setaf 4`
 magenta=`tput setaf 5`
+check="\xE2\x9C\x93"
+cross="X"
+colors=("${magenta}" "${red}" "${green}" "${cyan}" "${blue}")
+spin='-\|/'
 
 ########################################################
 # Parse out semver to int (eg. 18.24.123 -> 018024123
@@ -81,8 +85,51 @@ check_tools() {
   eval $__resultvar="'${result[@]}'"
 }
 
+install_status() {
+  local tool_name=$1 disp_char=$2 char_color=$3 fail_msg=$4
+  printf "\r${cyan}[${!char_color}${disp_char}${cyan}] Installing ${tool_name} ${red}${fail_msg}${reset}"
+}
+
 install_tools() {
   local distro=$(cat /etc/os-release | grep '^ID=' | awk -F= '{gsub (/"/, "", $2); print $2}')
+  local kernel=$(uname | awk '{print tolower($0)}')
+  local install_tools=("$@")
+  for t in $tools_
+  do
+    local tool=$(eval echo "\${t}_")
+    local name="${tool}name"
+    local failed_step=0
+    [[ ! " ${install_tools[*]} " =~ "${!name}" ]] && continue
+    install_status ${!name} " " "green"
+    local install_var="${tool}install_commands_"
+    [[ -z ${!install_var+set} ]] && \
+      install_status ${!name} "${cross}" "red" && \
+      echo && \
+      continue
+    [[ " ${!install_var} " =~ "${kernel}" ]] && install_steps="${install_var}${kernel}_"
+    [[ " ${!install_var} " =~ "${distro}" ]] && install_steps="${install_var}${distro}_"
+    for c in ${!install_steps}
+    do
+      eval "(${!c}) 2>/dev/null &"
+      pid=$!
+      while kill -0 $pid 2>/dev/null
+      do
+        i=$(( (i+1) %4 ))
+        install_status ${!name} "${spin:$i:1}" "cyan"
+        sleep .1
+      done
+      wait $pid
+      local exit_status=$?
+      if [[ ${exit_status} -ne 0 ]]
+      then
+        install_status ${!name} "${cross}" "red" "Install step ${c:0-1} failed: Exit code ${exit_status}"
+        failed_step=1
+        echo
+        break
+      fi
+    done
+    [[ $failed_step -ne 1 ]] && install_status ${!name} "${check}" "green" && echo
+  done
 }
 
 ########################################################
@@ -97,6 +144,15 @@ version_good() {
       return 0
     fi
     return 1
+}
+
+ask_to_install() {
+  local result="" __resultvar=$1
+  printf "${magenta}Install required software? (Y/n): ${reset}"
+  read result
+  [[ "${result}" == "" ]] && result="y"
+  result=$(echo ${result} | awk '{print tolower($0)}')
+  eval $__resultvar="'$result'"
 }
 
 usage=$(cat << EOM
@@ -115,11 +171,12 @@ Usage:
 EOM
 )
 main() {
-  local failed_software=()
+  local failed_software=() install=""
   current_version=0
   eval $(parse_yaml "${tools_yaml}")
   check_tools failed_software
-  echo ${failed_software[*]}
+  ask_to_install install
+  [[ "${install}" == "y" ]] && install_tools "${failed_software[@]}"
 }
 
 [[ $# -eq 0 ]] && printf "Missing options!\n\n${usage}\n"
